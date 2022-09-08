a70118f53d15532f13ff4228d1ddf0d19329cf3f
ogeh2001 <ogehsolomon2@gmail.com>
2022-09-08 15:50:57 -0700

    my 4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,9 +1,10 @@
 #include <stdio.h>
 
 /**
-* main - printing
-* Return: Always 0 (Succesful)
-*/
+ * main - Printing
+ *
+ * Return: Always 0 (Successful)
+ */
 int main (void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
