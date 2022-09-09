898cf3d109cd0467c1ae80548ab727e64d39cb9a
ogeh2001 <ogehsolomon2@gmail.com>
2022-09-08 18:07:02 -0700

    4-puts.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,6 +1,5 @@
 #include <stdio.h>
 #include <unistd.h>
-
 /**
  * main - print quote
  *
@@ -10,6 +9,6 @@
  */
 int main(void)
 {
-	Write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n",59);
+	Write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
 	return (1);
 }
