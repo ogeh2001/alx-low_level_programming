4a8dc9d1e7dd0a9fd1c86219588ae2bbb5641d18
ogeh2001 <ogehsolomon2@gmail.com>
2022-09-09 15:46:30 -0700

    0-positive_or_negative.c

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -6,8 +6,8 @@
  * main - Positive or Negative - print sign of random number
  * @void: Empty parameter list for main.
  *
- * Description: this if/else statement declares the sign (positive
- * or negative) of a random number. It also states if the value 
+ * Description: this if/else statement declares the sign (positive 
+ * or negative) of a random number. It also states if the value
  * is zero.
  *
  * Return: 0 for success
@@ -37,4 +37,4 @@ int main(void)
 	}
 
 	return (0);
-}	
+}
