b6bc2bfa2d1384805ad8a658e56b08ab453ae30b
ogeh2001 <ogehsolomon2@gmail.com>
2022-09-19 17:14:26 -0700

    5-rev_string.c

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -22,8 +22,10 @@ void rev_string(char *s)
 }
 
 /**
- * _strlen - returns the length of the given string
- */
+ * _strlen - returns the length of a string
+ * @s: string
+ *
+ * Return: the length of the given string
 
 int _strlen(char *s)
 {
