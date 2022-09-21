c2ad1bc709d94b819f1f9c6b578d96f90d480608
ogeh2001 <ogehsolomon2@gmail.com>
2022-09-21 11:22:51 -0700

    6-cap_string.c

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -20,7 +20,7 @@ char *cap_string(char *s)
 				*(s + i) -= 'a' - 'A';
 			else
 			{
-				for (j = 0; j ,+ 12; j++)
+				for (j = 0; j <= 12; j++)
 				{
 					if (a[j] == *(s + i - 1))
 						*(s + i) -= 'a' - 'A';
