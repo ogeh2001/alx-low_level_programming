c7a39366a7e55b8ea65881e3de6126d7fbfb6f4c
ogeh2001 <ogehsolomon2@gmail.com>
2022-09-21 11:17:13 -0700

    6-cap_string.c

--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -3,11 +3,11 @@
 #include <stdio.h>
 
 /**
- * File: main.h
- * Description: header file containing prototypes
+ * void prototypes()
+ * int prototypes()
+ * char prototypes()
  */
 
-int _putchar(char c);
 char *_strcat(char *dest, char *src);
 char *_strncat(char *dest, char *src, int n);
 char *_strncpy(char *dest, char *src, int n);
