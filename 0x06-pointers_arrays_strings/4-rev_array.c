6b2614a86c304ba21016b70f0480e9d41035d72c
ogeh2001 <ogehsolomon2@gmail.com>
2022-09-22 09:48:33 -0700

    bobo

--- 0x06-pointers_arrays_strings/4-rev_array.c
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * reverse_ray - Reverse the content of an array of integers.
+ * reverse_array - Reverse the content of an array of integers.
  * @a: the array of integers to be reversed.
  * @n: the number of elements in the array.
  */
@@ -10,7 +10,7 @@ void reverse_array(int *a, int n)
 {
 	int tmp, index;
 
-	for (index = n - 1; index >= n/2; index--)
+	for (index = n - 1; index >= n / 2; index--)
 	{
 		tmp = a[n - 1 - index];
 		a[n - 1 - index] = a[index];
