6f32eb82b41bcd11f656632484a629b5e42fd1c0
ogeh2001 <ogehsolomon2@gmail.com>
2022-09-22 11:11:47 -0700

    k

--- 0x06-pointers_arrays_strings/104-print_buffer.c
+++ 0x06-pointers_arrays_strings/104-print_buffer.c
@@ -16,7 +16,7 @@ void print_line(char *c, int s, int l)
 	for (j = 0; j <= 9; j++)
 	{
 		if (j <= s)
-			printf("%02X", c[1 * 10 + j]);
+			printf("%02X", c[l * 10 + j]);
 		else
 			printf(" ");
 		if (j % 2)
@@ -24,8 +24,8 @@ void print_line(char *c, int s, int l)
 	}
 	for (k = 0; k <= s; k++)
 	{
-		if (c[1 * 10 + k] > 31 && c[1 * 10 + k] < 127)
-			putchar(c[1 * 10 + k]);
+		if (c[l * 10 + k] > 31 && c[l * 10 + k] < 127)
+			putchar(c[l * 10 + k]);
 		else
 			putchar('.');
 	}
